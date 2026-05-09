66f407746bb86cc27b9c05c1162794e9a5e5443f
Hamilton Bello Rojas <hbrskates@gmail.com>
2026-05-09 04:10:24 -0300

    Initial commit

---
+++ Assets/_Solar System/Scripts/Test_one.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_one : MonoBehaviour
+{
+    int testInt = 0;
+    // This is a test script to check if the script is working or not. It doesn't do anything.
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
