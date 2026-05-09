c298354e92a7f9c50fe8e081b33e992457ba3914
Hamilton Bello Rojas <hbrskates@gmail.com>
2026-05-09 11:38:22 -0300

    Rotation Script and Skyboxes
    Minor rework of the rotation script, and addition of different skyboxes for further testing

--- Assets/_Solar System/Scripts/RotateAround.cs
+++ Assets/_Solar System/Scripts/RotateAround.cs
@@ -17,10 +17,17 @@ public class RotateAround : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void Update () {
-		// RotateAround takes three arguments, first is the Vector to rotate around
-		// second is a vector that axis to rotate around
-		// third is the degrees to rotate, in this case the speed per second
-		transform.RotateAround(target.transform.position,target.transform.up,speed * Time.deltaTime);
-	}
+	void Update (){
+
+        RotateObject();
+
+    }
+
+    private void RotateObject()
+    {
+        // RotateAround takes three arguments, first is the Vector to rotate around
+        // second is a vector that axis to rotate around
+        // third is the degrees to rotate, in this case the speed per second
+        transform.RotateAround(target.transform.position, target.transform.up, speed * Time.deltaTime);
+    }
 }
